933e64e27eee3e856d672816605eb71faacf3786
Johanssono <ottojohanssonjobb@gmail.com>
2023-12-19 16:02:05 +0100

    init

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.berry = {-subtheme
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
